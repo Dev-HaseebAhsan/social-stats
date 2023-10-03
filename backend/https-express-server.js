b3b8416e02257d2a5cf168974db4474cd757b0b0
Angelo Tolentino <the.angelo.tolentino@gmail.com>
2023-10-03 11:52:18 -0400

    working on osmosis integration

--- backend/https-express-server.js
+++ backend/https-express-server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+//Import osmosis, the instance is const osmosis
+const osmosis = require('osmosis');
+
 //index endpoint
 app.get("/", function(req, res){
     res.send("imagine getting this index page.");
@@ -12,6 +15,32 @@ app.get("/app", function(req,res){
     res.json({message: "received from express"});
 });
 
+app.get("/app/youtube", function(req,res){
+    osmosis.get()
+    res.json({});
+});
+
+app.get("/app/twitter", function(req, res){
+    res.json({});
+});
+
+app.get("/app/meta", function(req, res){
+    res.json({});
+});
+
+app.get("/app/twitch", function(req, res){
+    res.json({});
+});
+
+app.get("/app/kick", function(req, res){
+    res.json({});
+});
+
+app.get("/app/instagram", function(req,res){
+    res.json({});
+});
+
 app.listen(port, function(){
     console.log("listening on port " + port);
-})
+});
+
